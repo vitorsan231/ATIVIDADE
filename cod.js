ebd7d024d236cd00431fa46fd2b425841ee7680d
vitorsan231 <44277519+vitorsan231@users.noreply.github.com>
2018-10-28 14:32:54 -0300

    Update cod.js

--- cod.js
+++ cod.js
@@ -1,40 +1,51 @@
+function unique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
 var button = document.getElementById('mega');
-
 button.onclick = function() {
+	var aposta = [];
 	var a = new Array();
-	
-	for(var i=0;i<6;i++){
-		a[i] = Math.floor((Math.random() * 60) + 1);
-		document.getElementById("paragrafo").innerHTML = a;
+	var endereco = 0;
+	while (aposta.length != 6){
+		a[endereco] = Math.floor((Math.random() * 60) + 1);
+		endereco ++;
+		var aposta = a.filter(unique);
 	}	
+	document.getElementById("paragrafo").innerHTML = aposta;
 }
 var button1 = document.getElementById('quina');
-
 button1.onclick = function() {
+	var aposta = [];
 	var a = new Array();
-	
-	for(var i=0;i<5;i++){
-		a[i] = Math.floor((Math.random() * 80) + 1);
-		document.getElementById("paragrafo").innerHTML = a;
+	var endereco = 0;
+	while (aposta.length != 5){
+		a[endereco] = Math.floor((Math.random() * 80) + 1);
+		endereco ++;
+		var aposta = a.filter(unique);
 	}	
+	document.getElementById("paragrafo").innerHTML = aposta;	
 }
 var button2 = document.getElementById('lotomania');
-
 button2.onclick = function() {
+	var aposta = [];
 	var a = new Array();
-	var ran = Math.floor((Math.random() * 15) + 1);
-	for(var i=0;i<ran;i++){
-		a[i] = Math.floor((Math.random() * 100) + 1);
-		document.getElementById("paragrafo").innerHTML = a;
+	var endereco = 0;
+	while (aposta.length != 50){
+		a[endereco] = Math.floor((Math.random() * 99) + 1);
+		endereco ++;
+		var aposta = a.filter(unique);
 	}	
+	document.getElementById("paragrafo").innerHTML = aposta;	
 }
 var button3 = document.getElementById('lotofacil');
-
 button3.onclick = function() {
+	var aposta = [];
 	var a = new Array();
-	
-	for(var i=0;i<15;i++){
-		a[i] = Math.floor((Math.random() * 25) + 1);
-		document.getElementById("paragrafo").innerHTML = a;
+	var endereco = 0;
+	while (aposta.length != 15){
+		a[endereco] = Math.floor((Math.random() * 25) + 1);
+		endereco ++;
+		var aposta = a.filter(unique);
 	}	
+	document.getElementById("paragrafo").innerHTML = aposta;	
 }
